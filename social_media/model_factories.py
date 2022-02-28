5eb522b0e36a7fb653910b2cf9f6c730965bf877
sinhaugoh <sinhaugoh456@hotmail.com>
2022-02-28 23:31:24 +0800

    create factories for models

---
+++ social_media/model_factories.py
@@ -0,0 +1,30 @@
+import factory
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from .models import *
+
+class AppUserFactory(factory.django.DjangoModelFactory):
+    email = factory.Sequence(lambda n: 'user{}@email.com'.format(n))
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    password = factory.PostGenerationMethodCall('set_password', 'Asdf1234')
+
+
+    class Meta:
+        model = AppUser
+        
+
+class UserRelationshipFactory(factory.django.DjangoModelFactory):
+    user1 = factory.SubFactory(AppUserFactory)
+    user2 = factory.SubFactory(AppUserFactory)
+    
+    class Meta:
+        model = UserRelationship
+        
+
+class PostFactory(factory.django.DjangoModelFactory):
+    text = 'This is a status update text'
+    owner = factory.SubFactory(AppUserFactory)
+    image = factory.django.ImageField()
+    
+    class Meta:
+        model = Post
